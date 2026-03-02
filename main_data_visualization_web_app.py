b2a6801dfc4d610700557aec1852d74c5b9b0c0c
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-02 21:26:49 +0800

    Testing and configuration of streamlit web app

--- main_data_visualization_web_app.py
+++ main_data_visualization_web_app.py
@@ -1,8 +1,15 @@
 import os
 
 
-
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import streamlit
+import streamlit as st
+
+# Set page configuration (This allows you to edit the web app)
+st.set_page_config(page_title="Data Visualizer",
+                    layout="centered",page_icon="📊")
+
+# Title
+st.title("📊 Data Visualizer - Web App")
+
